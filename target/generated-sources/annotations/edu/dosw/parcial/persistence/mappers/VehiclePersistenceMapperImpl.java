71766745d969454e5713f73f3cac310ed043bc6b
Exael74 <stiven.pardo.g74@gmail.com>
2026-04-11 18:14:19 -0500

    feat: implement user authentication logic and update gitignore to exclude build artifacts

--- target/generated-sources/annotations/edu/dosw/parcial/persistence/mappers/VehiclePersistenceMapperImpl.java
+++ target/generated-sources/annotations/edu/dosw/parcial/persistence/mappers/VehiclePersistenceMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-04-11T17:38:27-0500",
+    date = "2026-04-11T18:13:50-0500",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.45.0.v20260224-0835, environment: Java 21.0.10 (Eclipse Adoptium)"
 )
 @Component
@@ -21,10 +21,10 @@ public class VehiclePersistenceMapperImpl implements VehiclePersistenceMapper {
 
         VehicleEntity.VehicleEntityBuilder vehicleEntity = VehicleEntity.builder();
 
-        vehicleEntity.vehicleId( vehicle.getVehicleId() );
-        vehicleEntity.licensePlate( vehicle.getLicensePlate() );
         vehicleEntity.brand( vehicle.getBrand() );
+        vehicleEntity.licensePlate( vehicle.getLicensePlate() );
         vehicleEntity.model( vehicle.getModel() );
+        vehicleEntity.vehicleId( vehicle.getVehicleId() );
 
         return vehicleEntity.build();
     }
@@ -38,8 +38,8 @@ public class VehiclePersistenceMapperImpl implements VehiclePersistenceMapper {
         Vehicle.VehicleBuilder vehicle = Vehicle.builder();
 
         vehicle.vehicleId( entity.getVehicleId() );
-        vehicle.licensePlate( entity.getLicensePlate() );
         vehicle.brand( entity.getBrand() );
+        vehicle.licensePlate( entity.getLicensePlate() );
         vehicle.model( entity.getModel() );
 
         return vehicle.build();
